4c63b716877e97ece6a2b0d6e946f39c43dedd33
hmz <hamza.jridi@esprit.tn>
2023-03-08 12:20:03 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,68 +1,17 @@
-import org.apache.commons.lang3.StringUtils;
-import org.junit.Test;
+ @Test
+    public void testMainMethod() throws Exception {
+        // GIVEN
+        String[] args = {"arg1", "arg2"};
+        Manager managerMock = Mockito.mock(Manager.class);
+        Mockito.doNothing().when(managerMock).exec(args);
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
 
-import java.io.FileNotFoundException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+        // WHEN
+        Manager.main(args);
 
-import static org.junit.Assert.*;
-
-public class TestManager {
-
-    private static final String[] VALID_ARGS = new String[]{"arg1", "arg2", "arg3", "arg4", "arg5", "arg6", "arg7", "arg8", "arg9"};
-
-    @Test
-    public void testPostExec_GivenValidArgsWithoutGenerateKeystore_WhenCalled_ThenReturnSameArgs() throws FileNotFoundException {
-        // GIVEN: des arguments valides sans l'argument "buildkeystore"
-        String[] args = VALID_ARGS.clone();
-
-        // WHEN: on appelle la méthode postExec avec les arguments
-        String[] result = Manager.postExec(args);
-
-        // THEN: la méthode doit retourner les mêmes arguments
-        assertArrayEquals(args, result);
+        // THEN
+        String expectedOutput = "ICO_STOPPED";
+        assertEquals(expectedOutput, outContent.toString().trim());
+        Mockito.verify(managerMock).exec(args);
     }
-
-    @Test
-    public void testPostExec_GivenValidArgsWithGenerateKeystore_WhenCalled_ThenReturnSameArgs() throws FileNotFoundException {
-        // GIVEN: des arguments valides avec l'argument "buildkeystore"
-        String[] args = new String[]{Manager.ARG_GENERATE_KEYSTORE};
-
-        // WHEN: on appelle la méthode postExec avec les arguments
-        String[] result = Manager.postExec(args);
-
-        // THEN: la méthode doit retourner les mêmes arguments
-        assertArrayEquals(args, result);
-    }
-
-    @Test
-    public void testPostExec_GivenValidArgsWithExistingStatusAndConfigurationFiles_WhenCalled_ThenReturnSubsetOfArgs() throws FileNotFoundException {
-        // GIVEN: des arguments valides avec des fichiers de statut et de configuration existants
-        String[] args = new String[]{"status_file_path", "config_file_path", "arg1", "arg2", "arg3", "arg4", "arg5", "arg6", "arg7", "arg8", "arg9"};
-
-        // WHEN: on appelle la méthode postExec avec les arguments
-        String[] result = Manager.postExec(args);
-
-        // THEN: la méthode doit retourner un sous-ensemble des arguments, sans le chemin des fichiers de statut et de configuration
-        assertEquals(args.length - 2, result.length);
-        assertTrue(StringUtils.isNotBlank(result[0]));
-        assertTrue(StringUtils.isNotBlank(result[1]));
-    }
-
-    @Test
-    public void testPostExec_GivenValidArgsWithMissingStatusOrConfigurationFiles_WhenCalled_ThenExitWithErrorCode() throws FileNotFoundException {
-        // GIVEN: des arguments valides avec un fichier de statut ou de configuration manquant
-        String[] args = new String[]{"missing_status_file_path", "missing_config_file_path", "arg1", "arg2", "arg3", "arg4", "arg5", "arg6", "arg7", "arg8", "arg9"};
-
-        // WHEN: on appelle la méthode postExec avec les arguments
-        try {
-            Manager.postExec(args);
-        } catch (Exception e) {
-            // THEN: la méthode doit quitter le programme avec un code d'erreur
-            assertEquals(-1, e.getMessage());
-        }
-    }
-}
