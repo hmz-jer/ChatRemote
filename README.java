969be13c870dc32eb4bc7528d6cf2158180c8f9e
hmz <hamza.jridi@esprit.tn>
2023-03-09 11:03:11 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,28 +1,43 @@
-import org.apache.commons.csv.CSVRecord;
-import org.junit.Test;
-import static org.junit.Assert.*;
+plugins {
+    id 'java'
+    id 'jacoco'
+}
 
-public class XPathConfigurationItemTest {
+jacoco {
+    toolVersion = "0.8.7"
+    reportsDir = file("$buildDir/reports/jacoco")
+    // Exclure certaines classes pour augmenter la couverture de code
+    excludes = [
+        '**/config/**', // Exclure les classes de configuration
+        '**/model/**', // Exclure les classes de modèles de données
+        '**/exceptions/**', // Exclure les classes d'exceptions
+        '**/utils/**', // Exclure les classes utilitaires
+        '**/enums/**', // Exclure les enums
+        '**/*Test*', // Exclure les classes de tests
+        '**/*TestSuite*', // Exclure les suites de tests
+        '**/*Steps*', // Exclure les classes de tests BDD (Behavior Driven Development)
+        '**/*Cucumber*', // Exclure les classes Cucumber
+    ]
+}
 
-    @Test
-    public void testValidRecord() throws InvalidConfigurationException {
-        CSVRecord record = new CSVRecord(new String[] {"message_lso", "message_id", "debtor_agent"});
-        XPathConfigurationItem item = new XPathConfigurationItem(record);
-        assertEquals("message_lso", item.getMessagelso());
-        assertEquals("message_id", item.getMessageId());
-        assertEquals("debtor_agent", item.getDebtorAgent());
+test {
+    jacoco {
+        // Inclure toutes les classes sauf celles exclues par Jacoco
+        excludes = jacoco.excludes
+        // Ajouter des inclusions si nécessaire
+        // includes = ['com.example.mypackage.*']
     }
+}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testIncompleteRecord() throws InvalidConfigurationException {
-        CSVRecord record = new CSVRecord(new String[] {"message_lso", "message_id"});
-        XPathConfigurationItem item = new XPathConfigurationItem(record);
+jacocoTestReport {
+    // Inclure toutes les classes sauf celles exclues par Jacoco
+    additionalSourceDirs.from = sourceSets.main.allSource.srcDirs
+    additionalClassDirs.from = files(sourceSets.main.output.classesDirs)
+    sourceDirectories.from = files(sourceSets.main.java.srcDirs)
+    classDirectories.from = files(sourceSets.main.output.classesDirs)
+    // Générer des rapports HTML et XML
+    reports {
+        xml.enabled = true
+        html.enabled = true
     }
-
-    @Test(expected = InvalidConfigurationException.class)
-    public void testEmptyFields() throws InvalidConfigurationException {
-        CSVRecord record = new CSVRecord(new String[] {"", "message_id", "debtor_agent"});
-        XPathConfigurationItem item = new XPathConfigurationItem(record);
-    }
-
 }
