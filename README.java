c9d438f88aa90b2ecfcddc44bbc1845cd06a03f8
hmz <hamza.jridi@esprit.tn>
2023-04-25 14:44:59 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,40 +1,4 @@
 
-Prérequis
-Avant de commencer à virtualiser les API Gateway d'Axway, vous devez vous assurer que vous avez installé les éléments suivants:
-
-Axway API Gateway
-Axway Policy Studio
-Étape 1: Créer un projet
-La première étape consiste à créer un projet pour votre API virtuelle. Pour ce faire, ouvrez Axway Policy Studio et créez un nouveau projet en sélectionnant "File" -> "New" -> "Project".
-
-Étape 2: Ajouter une API virtuelle
-Une fois que vous avez créé votre projet, la prochaine étape consiste à ajouter une API virtuelle. Pour ce faire, cliquez avec le bouton droit de la souris sur le dossier "APIs" de votre projet et sélectionnez "New API".
-
-Dans la fenêtre qui s'ouvre, entrez les détails de votre API, tels que son nom, sa description et sa version.
-
-Étape 3: Configurer votre API virtuelle
-Une fois que vous avez créé votre API virtuelle, la prochaine étape consiste à la configurer. Pour ce faire, double-cliquez sur votre API virtuelle pour ouvrir sa configuration.
-
-Dans la fenêtre de configuration, vous pouvez définir les paramètres tels que les routes, les points de terminaison, les politiques, etc.
-
-Étape 4: Tester votre API virtuelle
-Une fois que vous avez configuré votre API virtuelle, vous pouvez la tester en utilisant la fonction de test intégrée d'Axway Policy Studio. Pour ce faire, cliquez sur le bouton "Test" dans la barre d'outils.
-
-Étape 5: Déployer votre API virtuelle
-Une fois que vous êtes satisfait du fonctionnement de votre API virtuelle, la dernière étape consiste à la déployer. Pour ce faire, sélectionnez votre API virtuelle dans la vue de projet, cliquez avec le bouton droit de la souris dessus et sélectionnez "Deploy".
-
-Dans la fenêtre qui s'ouvre, sélectionnez le serveur sur lequel vous souhaitez déployer votre API virtuelle et cliquez sur "OK".
-
-Conclusion
-C'est tout ce qu'il y a à savoir pour virtualiser les API Gateway d'Axway en utilisant Axway Policy Studio. En suivant ces étapes, vous devriez être en mesure de créer et de déployer des API virtuelles de manière efficace.
-
-
-
-
-
-hamza.jeridi@outlook.com
-Non je veux en anglais 
-
 Sure, I'd be happy to help you create a tutorial document to assist developers in virtualizing Axway API Gateways. Here are the steps to follow:
 
 Prerequisites
