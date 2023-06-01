242f9f8a504663a298eb7592d36d29e3eb207ae1
hmz <hamza.jridi@esprit.tn>
2023-06-01 17:52:09 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,5 +1,31 @@
-dependencies {
-    implementation 'org.springframework.boot:spring-boot-starter-web'
-    implementation 'com.fasterxml.jackson.core:jackson-databind'
-    implementation 'com.fasterxml.jackson.dataformat:jackson-dataformat-csv'
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class PersonController {
+
+    @PostMapping("/persons")
+    public ResponseEntity<String> receiveJsonAndConvertToCsv(@RequestBody List<Person> persons) {
+        try {
+            CsvSchema schema = CsvSchema.builder().setUseHeader(true).build();
+            ObjectReader oReader = new CsvMapper().readerFor(Person.class).with(schema);
+
+            File csvFile = new File("persons.csv");
+            oReader.writeValues(csvFile).writeAll(persons).close();
+
+            return ResponseEntity.ok("File created successfully!");
+
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().body("Error processing JSON to CSV: " + e.getMessage());
+        }
+    }
 }
