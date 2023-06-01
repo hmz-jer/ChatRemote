94edfe6cb136b315438401bdc67e98a7182376dd
hmz <hamza.jridi@esprit.tn>
2023-06-01 17:48:25 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,23 +1,5 @@
-Step 11: Generate cURL Scripts from Postman
-
-Postman provides a convenient feature that allows you to generate cURL scripts for your requests. These scripts can be useful if you need to execute requests from the command line or integrate them into automated scripts. Here's how you can generate cURL scripts in Postman:
-
-1. In the Postman request tab, make sure the request you want to generate a cURL script for is selected.
-
-2. Click on the "Code" button located on the right side of the request details pane, just below the "Send" button.
-
-3. In the "Code Snippets" window that appears, select "cURL" from the list of available options.
-
-4. Postman will generate the cURL script for your request and display it in the code snippet area.
-
-5. Copy the generated cURL script by clicking on the "Copy to Clipboard" button.
-
-6. Now you can paste the copied cURL script into a terminal or a text editor of your choice.
-
-7. The generated cURL script includes all the necessary information to execute the request, including the HTTP method, URL, headers, parameters, and request body.
-
-8. Modify the cURL script as needed, such as updating the values of headers or parameters, to suit your specific requirements.
-
-9. Finally, execute the cURL script to send the request and receive the response from the API server.
-
-By generating cURL scripts from Postman, you can easily transfer your requests to command-line environments or incorporate them into your automation workflows. This enables seamless integration between your Postman testing and other parts of your development and testing processes.
+dependencies {
+    implementation 'org.springframework.boot:spring-boot-starter-web'
+    implementation 'com.fasterxml.jackson.core:jackson-databind'
+    implementation 'com.fasterxml.jackson.dataformat:jackson-dataformat-csv'
+}
