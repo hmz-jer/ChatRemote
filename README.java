7dd8e9548cdae6cd5c6f0063eb5a93dd56b484a1
hmz <hamza.jridi@esprit.tn>
2023-06-12 14:28:04 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,8 +1,38 @@
-#!/bin/bash
+!/bin/bash
 
-# Définir le chemin du dossier contenant les fichiers CSV et le fichier de sortie
-DOSSIER_CSV="/chemin/vers/le/dossier/csv"
-FICHIER_SORTIE="/chemin/vers/le/fichier/sortie.csv"
+# Définir le chemin du dossier contenant les fichiers CSV et les fichiers de sortie
+DOSSIER_CSV="/home/hmz/Documents/test"
+FICHIER_SORTIE_CONSUMER="/home/hmz/Documents/sortie_consumer.csv"
+FICHIER_SORTIE_PSP="/home/hmz/Documents/sortie_psp.csv"
+
+# Concaténer les fichiers en fonction de leur type
+function concatener {
+    local premier=true
+    local nb_colonnes=0
+    local motif=$1
+    local fichier_sortie=$2
+
+    for fichier in $(ls -v $DOSSIER_CSV/$motif)
+    do
+      if [ ! -f "$fichier" ] || [ ! -r "$fichier" ]; then
+          echo "Le fichier $fichier n'existe pas ou n'est pas lisible"
+          exit 1
+      fi
+
+      if $premier; then
+          nb_colonnes=$(head -1 $fichier | awk -F';' '{print NF}')
+          head -1 $fichier > $fichier_sortie
+          tail -n +2 $fichier | grep . >> $fichier_sortie
+          premier=false
+      else
+          if [ $nb_colonnes -ne $(head -1 $fichier | awk -F';' '{print NF}') ]; then
+              echo "Les fichiers n'ont pas le même nombre de colonnes."
+              exit 1
+          fi
+          tail -n +2 $fichier | grep . >> $fichier_sortie
+      fi
+    done
+}
 
 # Vérifier si le dossier existe et est lisible
 if [ ! -d "$DOSSIER_CSV" ] || [ ! -r "$DOSSIER_CSV" ]; then
@@ -10,34 +40,6 @@ if [ ! -d "$DOSSIER_CSV" ] || [ ! -r "$DOSSIER_CSV" ]; then
     exit 1
 fi
 
-# Trouver tous les fichiers .csv dans le dossier donné, sauter la première ligne (l'en-tête)
-# pour tous les fichiers après le premier et concaténer dans le fichier de sortie
-premier=true
-nb_colonnes=0
-for fichier in $DOSSIER_CSV/*.csv
-do
-  # Vérifier si le fichier existe et est lisible
-  if [ ! -f "$fichier" ] || [ ! -r "$fichier" ]; then
-      echo "Le fichier $fichier n'existe pas ou n'est pas lisible"
-      exit 1
-  fi
-
-  # Vérifier si tous les fichiers ont le même nombre de colonnes
-  if $premier; then
-      nb_colonnes=$(head -1 $fichier | awk -F';' '{print NF}')
-  else
-      if [ $nb_colonnes -ne $(head -1 $fichier | awk -F';' '{print NF}') ]; then
-          echo "Les fichiers n'ont pas le même nombre de colonnes."
-          exit 1
-      fi
-  fi
-
-  if $premier; then
-     # Prendre l'en-tête du premier fichier:
-     head -1 $fichier > $FICHIER_SORTIE
-     tail -n +2 $fichier | grep . >> $FICHIER_SORTIE
-     premier=false
-  else
-     tail -n +2 $fichier | grep . >> $FICHIER_SORTIE
-  fi
-done
+# Appeler la fonction de concaténation pour chaque type de fichier
+concatener "*client-info.csv" $FICHIER_SORTIE_CONSUMER
+concatener "*psp-list.csv" $FICHIER_SORTIE_PSP
