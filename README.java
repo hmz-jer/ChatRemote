65442268c59d0c252a6bc480d0acade98a715f6e
hmz <hamza.jridi@esprit.tn>
2023-06-13 11:46:14 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,64 +1,24 @@
-#!/bin/bash
+# CSV Concatenation Script
 
-# Définir le chemin du dossier contenant les fichiers CSV et les fichiers de sortie
-DOSSIER_CSV_CONSUMER="/chemin/vers/le/dossier/consumer/csv"
-DOSSIER_CSV_PSP="/chemin/vers/le/dossier/psp/csv"
-FICHIER_SORTIE_CONSUMER="/chemin/vers/le/fichier/sortie_consumer.csv"
-FICHIER_SORTIE_PSP="/chemin/vers/le/fichier/sortie_psp.csv"
+This script concatenates all CSV files in a specified directory. It has been designed to work with two types of CSV files: 'consumer-info' and 'psp-list'.
 
-# Définir le chemin vers les clés et certificats
-CLE_PUBLIQUE="/chemin/vers/la/clé/publique"
-CLE_PRIVEE="/chemin/vers/la/clé/privée"
-CERTIFICAT="/chemin/vers/le/certificat"
+The script reads all CSV files in a directory, concatenates them while preserving the header of the first file, and outputs the concatenated content into a new file. It then signs and encrypts the file before moving it to the destination folder.
 
-# Concaténer les fichiers en fonction de leur type
-function concatener {
-    local premier=true
-    local nb_colonnes=0
-    local dossier=$1
-    local fichier_sortie=$2
+## Configuration
 
-    for fichier in $(ls -v $dossier/*.csv)
-    do
-      if [ ! -f "$fichier" ] || [ ! -r "$fichier" ]; then
-          echo "Le fichier $fichier n'existe pas ou n'est pas lisible"
-          exit 1
-      fi
+The script reads its configuration from a file named `config.cfg`. In this file, you must define the following variables:
 
-      if $premier; then
-          nb_colonnes=$(head -1 $fichier | awk -F';' '{print NF}')
-          head -1 $fichier > $fichier_sortie
-          tail -n +2 $fichier | grep . >> $fichier_sortie
-          premier=false
-      else
-          if [ $nb_colonnes -ne $(head -1 $fichier | awk -F';' '{print NF}') ]; then
-              echo "Les fichiers n'ont pas le même nombre de colonnes."
-              exit 1
-          fi
-          tail -n +2 $fichier | grep . >> $fichier_sortie
-      fi
-    done
+- `DOSSIER_CSV_CONSUMER`: The path to the directory containing consumer CSV files.
+- `DOSSIER_CSV_PSP`: The path to the directory containing PSP CSV files.
+- `DOSSIER_DONE`: The path to the directory where the processed files will be moved to.
+- `DOSSIER_ERREUR`: The path to the directory where the files with errors will be moved to.
+- `KEY_PEM`: The path to your PEM private key.
+- `CER_PEM`: The path to your PEM public key.
 
-    # Signer le fichier en utilisant smime avec l'option -outform PEM
-    openssl smime -sign -binary -in $fichier_sortie -signer $CERTIFICAT -inkey $CLE_PRIVEE -outform PEM -out $fichier_sortie.pem
+## Usage
 
-    # Chiffrer le fichier signé avec la clé publique du récepteur, en utilisant l'option -outform PEM
-    openssl smime -encrypt -binary -aes256 -outform PEM $CLE_PUBLIQUE < $fichier_sortie.pem > $fichier_sortie.enc
+Run the script by typing `./csv_concatenation.sh` in your terminal.
 
-    rm $fichier_sortie $fichier_sortie.pem
-}
+## Error Handling
 
-# Vérifier si le dossier existe et est lisible
-if [ ! -d "$DOSSIER_CSV_CONSUMER" ] || [ ! -r "$DOSSIER_CSV_CONSUMER" ]; then
-    echo "Le dossier $DOSSIER_CSV_CONSUMER n'existe pas ou n'est pas lisible"
-    exit 1
-fi
-
-if [ ! -d "$DOSSIER_CSV_PSP" ] || [ ! -r "$DOSSIER_CSV_PSP" ]; then
-    echo "Le dossier $DOSSIER_CSV_PSP n'existe pas ou n'est pas lisible"
-    exit 1
-fi
-
-# Appeler la fonction de concaténation pour chaque type de fichier
-concatener $DOSSIER_CSV_CONSUMER $FICHIER_SORTIE_CONSUMER
-concatener $DOSSIER_CSV_PSP $FICHIER_SORTIE_PSP
+If there is an error with a file (for example, if it does not exist, is not readable, or does not have the same number of columns), the script will move this file to the error directory defined in the configuration file. The script also checks if the directories specified in the configuration file exist and are readable, and will terminate if this is not the case.
