8ac5aae65d9e9d49e5c3acb6afd0d28fc15b9215
hmz <hamza.jridi@esprit.tn>
2023-06-13 14:57:09 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,82 +1,56 @@
-#!/bin/bash
+# CSV File Concatenation, Signing, and Encryption Script
 
-# Lire le fichier de configuration
-source /chemin/vers/config.cfg
+This script is designed to concatenate, sign, and encrypt .csv files. It reads all .csv files from two input directories, concatenates them separately based on the file type, and writes the output to new .csv files in an output directory. Each output file is then signed and encrypted using OpenSSL. 
 
-# Vérifier si les variables nécessaires sont définies
-if [ -z "$DOSSIER_CSV_CONSUMER" ] || [ -z "$DOSSIER_CSV_PSP" ] || [ -z "$DOSSIER_DONE" ] || [ -z "$DOSSIER_ERREUR" ] || [ -z "$KEY_PEM" ] || [ -z "$CER_PEM" ]; then
-    echo "Erreur : Les variables nécessaires ne sont pas définies dans le fichier de configuration."
-    exit 1
-fi
+## Getting Started
 
-# Vérifier si les dossiers existent et sont lisibles
-for DOSSIER in $DOSSIER_CSV_CONSUMER $DOSSIER_CSV_PSP $DOSSIER_DONE $DOSSIER_ERREUR
-do
-    if [ ! -d "$DOSSIER" ] || [ ! -r "$DOSSIER" ]; then
-        echo "Erreur : Le dossier $DOSSIER n'existe pas ou n'est pas lisible."
-        exit 1
-    fi
-done
+Before running the script, you need to have OpenSSL installed on your system. If it's not already installed, you can download it from the official OpenSSL website: https://www.openssl.org/source/. 
 
-echo "Début du traitement des fichiers CSV..."
+You will also need to create a configuration file `config.cfg` with the following format:
 
-# Fonction pour concaténer, signer et chiffrer les fichiers CSV
-function traiter {
-    local dossier=$1
-    local motif=$2
-    local fichier_sortie="$DOSSIER_DONE/$(date +%Y%m%d%H%M%S).$motif"
-    local premier=true
-    local nb_colonnes=0
+```bash
+DOSSIER_CSV_CONSUMER="/path/to/consumer/csv/directory"
+DOSSIER_CSV_PSP="/path/to/psp/csv/directory"
+DOSSIER_DONE="/path/to/output/directory"
+DOSSIER_ERREUR="/path/to/error/directory"
+KEY_PEM="/path/to/your/private/key.pem"
+CER_PEM="/path/to/your/certificate/cer.pem"
+```
 
-    echo "Traitement des fichiers $motif dans le dossier $dossier..."
+Replace the `/path/to/...` with your actual directory paths and certificate files.
 
-    for fichier in $(ls -v $dossier/$motif)
-    do
-        if [ ! -f "$fichier" ] || [ ! -r "$fichier" ]; then
-            echo "Erreur : Le fichier $fichier n'existe pas ou n'est pas lisible."
-            mv "$fichier" "$DOSSIER_ERREUR"
-            echo "Le fichier $fichier a été déplacé vers le dossier ERROR."
-            continue
-        fi
+## Generating RSA Key Pair
 
-        echo "Concaténation du fichier $fichier..."
+You can generate your RSA key pair (private key and certificate) using the following command:
 
-        if $premier; then
-            nb_colonnes=$(head -1 $fichier | awk -F';' '{print NF}')
-            head -1 $fichier > $fichier_sortie
-            tail -n +2 $fichier | grep . >> $fichier_sortie
-            premier=false
-        else
-            if [ $nb_colonnes -ne $(head -1 $fichier | awk -F';' '{print NF}') ]; then
-                echo "Erreur : Les fichiers n'ont pas le même nombre de colonnes."
-                mv "$fichier" "$DOSSIER_ERREUR"
-                echo "Le fichier $fichier a été déplacé vers le dossier ERROR."
-                continue
-            fi
-            tail -n +2 $fichier | grep . >> $fichier_sortie
-        fi
+```bash
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cer.pem -sha256 -days 365 -subj '/CN=test'
+```
 
-        echo "Le fichier $fichier a été traité et supprimé."
-        rm $fichier
-    done
+You will be prompted to create a password. Remember this password as you will need it to decrypt the files later.
 
-    # Vérifier si le fichier de sortie est vide
-    if [ ! -s $fichier_sortie ]; then
-        echo "Le fichier $fichier_sortie est vide. Il ne sera pas signé ni chiffré."
-        rm $fichier_sortie
-        return
-    fi
+## Running the Script
 
-    echo "Signature et chiffrement du fichier $fichier_sortie..."
+Once you have all the necessary files and configurations, you can run the script as follows:
 
-    openssl smime -sign -signer $CER_PEM -inkey $KEY_PEM -in $fichier_sortie -out $fichier_sortie.signed -outform PEM -nodetach
-    openssl smime -encrypt -binary -aes-256-cbc -in $fichier_sortie.signed -out $fichier_sortie.enc -outform DER $CER_PEM
-    echo "Le fichier $fichier_sortie a été signé et chiffré."
-    rm $fichier_sortie $fichier_sortie.signed
-}
+```bash
+bash script.sh
+```
 
-# Appeler la fonction de traitement pour chaque type de fichier
-traiter $DOSSIER_CSV_CONSUMER "*consumer-info.csv"
-traiter $DOSSIER_CSV_PSP "*psp-list.csv"
+The script will process the CSV files, and you will see log messages on the console showing the progress.
 
-echo "Fin du traitement des fichiers CSV."
+## Decrypting the Files
+
+To decrypt the signed and encrypted files, use the following commands:
+
+```bash
+openssl smime -decrypt -in file.enc -binary -inform DER -inkey key.pem -out decrypted_file.csv
+```
+
+You will need to enter the password that you created when generating the RSA key pair.
+
+## Error Handling
+
+The script includes basic error handling. If an error occurs while reading a file, that file will be moved to the error directory specified in the configuration file. Similarly, if an output file is empty (i.e., there were no valid input files), the script will not sign or encrypt that file.
+
+Please make sure that all directories and files used by the script are readable and writable by the user running the script.
