715dfae3312704bc8967f38fa4a86b6749c31cc4
hmz <hamza.jridi@esprit.tn>
2023-08-25 14:54:50 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,16 +1,1 @@
-Objet : Explication du fonctionnement de l'application SOPA
-
-Bonjour,
-
-Je voulais vous donner une meilleure compréhension de l'application SOPA. D'abord, elle utilise Spring Actuator pour envoyer des rapports de statut de l'application, par exemple, le type 'UP' indique que l'application fonctionne correctement.
-
-Ce qui est important à noter, c'est que le statut de SOPA dépend d'ICON. SOPA fait un appel REST à ICON, en particulier à l'URL "/iris/status", pour déterminer son propre statut.
-
-SOPA sait à quelle instance de ICON se connecter en consultant un fichier de propriétés contenant une liste d'adresses IP. L'adresse spécifique à utiliser est spécifiée dans la propriété 'iris.server[0].port'.
-
-En fonction des résultats de l'appel à ICON, SOPA détermine son propre rapport de statut. Si elle n'obtient aucune erreur ('KO') et aucune exception, elle renvoie 'OK'. Si tous les résultats sont en erreur, elle renvoie 'KO'.
-
-Dans les autres cas, SOPA fait la somme des appels et calcule le pourcentage d'échecs. Ce pourcentage donne une indication sur l'état de santé global de l'application.
-
-Si vous avez des questions ou besoin de plus de détails, n'hésitez pas à me le faire savoir.
-
+find / -type f -not -path "/dev/*" -exec du -h 2>/dev/null {} + | sort -rh | head -1
