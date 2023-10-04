00a27b3819145ef1e31e4b0813f6a968016c2b4e
hmz <hamza.jridi@esprit.tn>
2023-10-04 09:53:52 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,31 +1,38 @@
-Bien sûr. Voici un exemple de mail d'estimation pour cinq tâches dont vous m'avez fourni le chiffrage :
+plugins {
+	id 'java'
+	id 'org.springframework.boot' version '2.7.16'
+	id 'io.spring.dependency-management' version '1.0.15.RELEASE'
+}
 
----
+group = 'com.exampleTSP'
+version = '0.0.1-SNAPSHOT'
 
-Objet : Estimation des tâches - Projet X
+java {
+	sourceCompatibility = '1.8'
+}
 
-Bonjour [Nom du destinataire],
+configurations {
+	compileOnly {
+		extendsFrom annotationProcessor
+	}
+}
 
-Suite à notre discussion, voici l'estimation chiffrée des cinq tâches dont nous avons discuté :
+repositories {
+	mavenCentral()
+}
 
-1. **Tâche A** : 10 heures
-2. **Tâche B** : 5 heures
-3. **Tâche C** : 15 heures
-4. **Tâche D** : 7 heures
-5. **Tâche E** : 3 heures
+dependencies {
+	implementation 'org.springframework.boot:spring-boot-starter-batch'
+	implementation 'org.springframework.boot:spring-boot-starter-data-cassandra'
+	implementation 'org.springframework.boot:spring-boot-starter-web'
+	compileOnly 'org.projectlombok:lombok'
+	developmentOnly 'org.springframework.boot:spring-boot-devtools'
+	annotationProcessor 'org.projectlombok:lombok'
+	testImplementation 'org.springframework.boot:spring-boot-starter-test'
+	testImplementation 'org.springframework.batch:spring-batch-test'
+	implementation 'org.springdoc:springdoc-openapi-ui:1.7.0'
+}
 
-**Total** : 40 heures
-
-Ce chiffrage a été établi en prenant en compte nos précédentes expériences et les spécificités que vous m'avez communiquées. Si des éléments devaient évoluer ou si des imprévus survenaient, je vous tiendrais bien sûr informé(e) pour réajuster ces estimations si nécessaire.
-
-Merci de me confirmer si ces estimations vous conviennent ou si vous souhaitez apporter des modifications.
-
-Je reste à votre disposition pour toute question ou clarification.
-
-Bien cordialement,
-
-[Votre nom]
-
----
-
-Notez que ce n'est qu'un exemple générique. Vous devriez adapter le contenu en fonction des détails spécifiques de votre projet et des informations fournies par votre destinataire.
+tasks.named('test') {
+	useJUnitPlatform()
+}
