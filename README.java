d8e6996b5b490c8581ecb7a5058cf2f30e86296a
hmz <hamza.jridi@esprit.tn>
2023-10-12 14:50:20 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,21 +1,37 @@
-    Objet : Demande de congés et proposition d'organisation
+D'accord, voici une modification de votre e-mail avec l'ajout de cette information :
 
-Bonjour [Nom du chef],
+---
 
-J’espère que tu vas bien. J'aimerais discuter avec toi de ma planification de congés pour le reste de l’année 2023.
+**Objet :** Rapport sur les tests de l'Outil de Test de Connectivité
 
-J’ai calculé et, en prenant en compte mes jours travaillés jusqu’à maintenant, je vais atteindre 213 jours de travail cette année si je m'organise comme suit :
+---
 
-- Prendre deux jours en novembre, soit le 11 et le 13.
-- Prendre le reste de mes congés du 13 décembre jusqu'à la fin de l’année.
+Cher(e) [Nom du destinataire],
 
-Cela me permet de contribuer au mieux aux projets en cours tout en respectant le calendrier prévu.
+J'espère que vous allez bien. Je me permets de vous adresser le rapport relatif aux tests effectués sur l'outil de test de connectivité. Vous trouverez ci-dessous une synthèse des méthodes utilisées et des résultats obtenus.
 
-Je suis convaincu que nous pouvons assurer une transition douce et efficace pour mon absence avec une planification adéquate. Si tu es d’accord avec cette organisation, je m’assurerai que toutes mes responsabilités soient bien gérées et transmises à l’équipe pendant mon absence.
+**Déploiement**
 
-Je reste bien sûr disponible pour en discuter plus amplement si tu souhaites ajuster quelque chose.
+Avant d'entamer les tests, nous avons déployé l'application sur le serveur 42. Cela nous a permis de simuler un environnement réel et d'assurer une évaluation précise de la performance et de la fiabilité de l'outil.
 
-Je te remercie pour ta compréhension et j’attends ton retour.
+**1. Test de Simulation-Request**
+
+Ce test a été conçu pour simuler des échanges de messages entre le client et le serveur. Les commandes `client-sign-message`, `client-encrypt-message`, `server-decrypt-message`, et `server-check-and-get-payload` ont été utilisées. [Insérer l'impression d'écran correspondante ici.]
+
+**2. Test de Simulation-Notification**
+
+Ce test simule une notification initiée par le serveur et traitée par le client. Les commandes utilisées incluent `server-sign-message`, `server-encrypt-message`, `client-decrypt-message`, et `client-check-and-get-payload`. [Insérer l'impression d'écran correspondante ici.]
+
+**Remarques :** Lors de nos tests, nous avons observé que, bien que les messages soient correctement déchiffrés, les espaces présents dans les messages d'origine sont supprimés lors du processus de déchiffrement. C'est un aspect que nous devrions examiner de plus près.
+
+Je vous invite à examiner les détails des tests et les impressions d'écran jointes. Vos commentaires et retours seront grandement appréciés. Si des ajustements ou des tests supplémentaires sont nécessaires, n'hésitez pas à me le faire savoir.
+
+Merci pour votre attention, et je reste à votre disposition pour toute information complémentaire.
 
 Cordialement,
-[Ton prénom]
+
+[Votre nom]
+
+---
+
+Vous pouvez désormais ajouter les impressions d'écran et envoyer cet e-mail à l'aide de votre client de messagerie préféré. Si vous avez d'autres ajustements ou questions, n'hésitez pas à me le faire savoir !
