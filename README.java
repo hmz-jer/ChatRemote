1e1f4cf80dadd44f42b0f325daa71875df5e5dd2
hmz <hamza.jridi@esprit.tn>
2023-10-18 19:57:02 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,47 +1,19 @@
-Bien entendu. Voici une version mise à jour de votre e-mail avec l'ajout de cette section :
+public Availablestatus gotavallablestatus (Stoppable. Type type)
 
----
+AvailableStatus clientstatus = null;
 
-**Objet :** Rapport sur les tests de l'Outil de Test de Connectivité
+Supplierstream<toppable>> streamSupplier()-> this.getBunningThreads().stream(), Eltortstoppable> typu.equals(stoppable.getType()))) Map<AvailableStatus, Long> client@tatusCount = atroan applier.get().collect (Calleats.groupingly troppablelistatus, Collectors.cousting 0)) string napaafting clientstatumCount.keyset().stream().map(key-> key+client.StatusCount.get(key)).collect (Collectors.joining("
 
----
+Long countRunning allentstatusCount.get(Avaliablestatus.STARTED) 7
 
-Cher(e) [Nom du destinataire],
+LE(countRunning fe null & countRunning-Intvalue() & countRunning, intValue() stromupplier.get(). (1) ( client status AvailableStatus.DEGRADED
 
-J'espère que vous allez bien. Je me permets de vous adresser le rapport relatif aux tests effectués sur l'outil de test de connectivité. Vous trouverez ci-dessous une synthèse des méthodes utilisées et des résultats obtenus.
+else if (countRunning null 11 couns Running.intValue())
 
-**Déploiement**
+clientstaton Availabletus.STOPPED
 
-Avant d'entamer les tests, nous avons déployé l'application sur le serveur 42. Cela nous a permis de simuler un environnement réel et d'assurer une évaluation précise de la performance et de la fiabilité de l'outil.
+) else ( clientStatus = Avaliablestatur.STARTED
 
-**1. Test de Simulation-Request**
+LOGGER, trace()-> (9 mapasstring, clientstatus.name());
 
-Ce test a été conçu pour simuler des échanges de messages entre le client et le serveur. Les commandes `client-sign-message`, `client-encrypt-message`, `server-decrypt-message`, et `server-check-and-get-payload` ont été utilisées. [Insérer l'impression d'écran correspondante ici.]
-
-**2. Test de Simulation-Notification**
-
-Ce test simule une notification initiée par le serveur et traitée par le client. Les commandes utilisées incluent `server-sign-message`, `server-encrypt-message`, `client-decrypt-message`, et `client-check-and-get-payload`. [Insérer l'impression d'écran correspondante ici.]
-
-**3. Vérification des Certificats**
-
-Nous avons également procédé à la vérification de la validité des certificats suivants :
-- `certificat_jws`
-- `certificate_jwe`
-- `server/keystoreACRoot.jks`
-- `client/keystoreACRoot.jks`
-
-Des impressions d'écran illustrant les résultats et les détails de ces vérifications sont jointes. [Insérer les impressions d'écran correspondantes ici.]
-
-**Remarques :** Lors de nos tests, nous avons observé que, bien que les messages soient correctement déchiffrés, les espaces présents dans les messages d'origine sont supprimés lors du processus de déchiffrement. C'est un aspect que nous devrions examiner de plus près.
-
-Je vous invite à examiner les détails des tests et les impressions d'écran jointes. Vos commentaires et retours seront grandement appréciés. Si des ajustements ou des tests supplémentaires sont nécessaires, n'hésitez pas à me le faire savoir.
-
-Merci pour votre attention, et je reste à votre disposition pour toute information complémentaire.
-
-Cordialement,
-
-[Votre nom]
-
----
-
-Avec cet ajout, le rapport est encore plus complet. Vous pouvez insérer vos impressions d'écran où indiqué et envoyer l'e-mail via votre client de messagerie. Si vous avez besoin d'autres modifications ou questions, n'hésitez pas à m'en faire part !
+return client patungs
