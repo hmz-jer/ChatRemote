a4cb0defa263affbd3bcbe63283324e503e14848
hmz <hamza.jridi@esprit.tn>
2023-10-19 15:01:49 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,26 +1,43 @@
-    private YourClassName yourClassInstance;
+    D'accord, voici un exemple de script shell que vous pouvez utiliser :
 
-    @Before
-    public void setup() {
-        yourClassInstance = Mockito.spy(new YourClassName());
-    }
+```bash
+#!/bin/bash
 
-    @Test
-    public void testGetAvailableStatus() {
-        // Mock getRunningThreads()
-        Stoppable mockStoppable1 = Mockito.mock(Stoppable.class);
-        Stoppable mockStoppable2 = Mockito.mock(Stoppable.class);
-        List<Stoppable> mockList = Arrays.asList(mockStoppable1, mockStoppable2);
+# Nom du script : nomfichier.sh
 
-        when(mockStoppable1.getType()).thenReturn(Stoppable.Type.YOUR_TYPE);
-        when(mockStoppable1.getStatus()).thenReturn(AvailableStatus.STARTED);
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 setadminstatus {0|1}"
+    exit 1
+fi
 
-        when(mockStoppable2.getType()).thenReturn(Stoppable.Type.YOUR_TYPE);
-        when(mockStoppable2.getStatus()).thenReturn(AvailableStatus.STARTED);
+command=$1
+value=$2
 
-        when(yourClassInstance.getRunningThreads()).thenReturn(mockList);
+if [ "$command" != "setadminstatus" ]; then
+    echo "La commande n'est pas reconnue."
+    exit 2
+fi
 
-        // Test
-        AvailableStatus result = yourClassInstance.getAvailableStatus(Stoppable.Type.YOUR_TYPE);
-        assertEquals(AvailableStatus.STARTED, result);
-    }
+case $value in
+    0)
+        echo "Arrêt de l'application..."
+        # Commande pour arrêter l'application ici
+        ;;
+    1)
+        echo "Redémarrage de l'application..."
+        # Commande pour redémarrer l'application ici
+        ;;
+    *)
+        echo "Valeur non reconnue. Utilisez 0 pour arrêter et 1 pour redémarrer."
+        exit 3
+        ;;
+esac
+
+exit 0
+```
+
+Remarques:
+
+1. Vous devez remplacer les commentaires `# Commande pour arrêter l'application ici` et `# Commande pour redémarrer l'application ici` par les commandes réelles pour arrêter et redémarrer votre application.
+2. Assurez-vous de donner les permissions d'exécution à ce script après l'avoir créé. Vous pouvez le faire avec la commande : `chmod +x nomfichier.sh`.
+3. Pour exécuter le script : `./nomfichier.sh setadminstatus 0` pour arrêter et `./nomfichier.sh setadminstatus 1` pour redémarrer.
