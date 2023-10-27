46c1e2fb38b4d4253e3865e2b7cee2e18144ef02
hmz <hamza.jridi@esprit.tn>
2023-10-27 09:41:20 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,2 +1,20 @@
+Bonjour à tous,
 
-     Bonjour [Nom du destinataire],Je suis heureux de vous informer que le problème lié à l'IDM a été corrigé avec succès. En collaboration étroite avec Nafaa et l'équipe Axway, nous avons pu identifier et résoudre les enjeux. La merge request (MR) associée a été minutieusement examinée et est maintenant validée.Vous pouvez dès à présent fusionner la MR en toute confiance.Je tiens à remercier Nafaa et toute l'équipe Axway pour leur dévouement et leur expertise qui ont grandement contribué à la résolution rapide de ce problème.Si vous avez des questions ou si vous avez besoin d'informations supplémentaires, n'hésitez pas à me contacter.Cordialement,
+Je voulais vous faire un récapitulatif rapide de notre daily meeting de ce matin :
+
+    EPI :
+        Les fichiers de configuration de la partie IDM ont été mis à jour. L'équipe BUILD a effectué les tests nécessaires et tout fonctionne correctement. Le problème rencontré précédemment a été corrigé et un email à ce sujet a déjà été envoyé pour vous en informer.
+
+    SCFP :
+        Nous travaillons aujourd'hui sur la QC 26852. Toute collaboration ou information supplémentaire à ce sujet est la bienvenue.
+
+    Interactions avec d'autres équipes :
+        L'équipe TSP a rencontré des difficultés pour envoyer des requêtes à l'API Gateway. Pour résoudre ce problème, nous avons redémarré l'instance de l'API Gateway, ce qui les a débloqués. N'hésitez pas à nous faire part de tout autre problème lié à cela.
+
+    Informations diverses :
+        Le VDI fonctionne sans accroc aujourd'hui.
+        Comme communiqué précédemment, ANIS sera en congé la semaine prochaine. Veuillez planifier vos besoins en conséquence.
+
+Je reste à votre disposition pour toute question ou clarification supplémentaire. N'oubliez pas de communiquer tout changement ou préoccupation à temps pour garantir une coordination fluide entre nos équipes.
+
+Bonne journée à tous,
