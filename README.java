7ac1e8964e8d6007c090db6d3566dfca17a493a0
hmz <hamza.jridi@esprit.tn>
2023-11-17 11:38:59 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,18 @@
-J'espère que vous allez bien. Je voulais vous demander si je pourrais exceptionnellement faire du télétravail mardi prochain après-midi. Un technicien doit passer chez moi, et je dois être présent pour cela. Je serai disponible et joignable par Teams et téléphone pendant cette période. Merci beaucoup pour votre compréhension.
+D'accord, voici une version plus souple et concise du mail :
+
+---
+
+Objet : Demande de Télétravail pour Mardi
+
+Salut [Nom de votre chef],
+
+J'espère que tu vas bien. Je voulais te demander si je peux travailler à distance ce mardi. Pour compenser, je viendrai au bureau jeudi. 
+
+Merci d'avance pour ta compréhension. N'hésite pas si tu as des questions.
+
+Cordialement,
+[Votre nom]
+
+---
+
+Ce mail est plus décontracté et va droit au but, tout en restant professionnel.
