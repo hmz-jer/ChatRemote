ac15b6172ec9b7ca02a3d690ee53c330ee5a51fc
hmz <hamza.jridi@esprit.tn>
2023-12-07 10:44:18 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,40 +1,5 @@
-Pour réaliser cette tâche en Java 8, vous pouvez suivre les étapes suivantes :
-
-1. **Extraire le nom du fichier du chemin d'entrée :**
-   Utilisez la classe `Paths` pour créer un objet `Path` à partir de votre chemin de fichier, puis utilisez la méthode `getFileName()` pour obtenir le nom du fichier.
-
-2. **Obtenir la date actuelle :**
-   Utilisez la classe `LocalDate` de Java 8 pour obtenir la date du jour.
-
-3. **Construire le nouveau chemin :**
-   Concaténez le nom du fichier (sans l'extension `.csv`), la date du jour, et ajoutez de nouveau l'extension `.csv`.
-
-Voici un exemple de code qui implémente ces étapes :
-
-```java
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-public class Main {
-    public static void main(String[] args) {
-        String inputPath = "chemin/vers/votre/fichier.csv"; // Remplacez par votre chemin
-        Path path = Paths.get(inputPath);
-        
-        // Extraire le nom du fichier sans l'extension
-        String fileName = path.getFileName().toString();
-        fileName = fileName.substring(0, fileName.lastIndexOf('.'));
-
-        // Obtenir la date actuelle
-        String currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-
-        // Construire le nouveau chemin
-        String newPath = fileName + "_" + currentDate + ".csv";
-
-        System.out.println("Nouveau chemin : " + newPath);
-    }
-}
-```
-
-Ce code générera un nouveau chemin avec le nom du fichier original, la date du jour formatée en `yyyyMMdd`, et l'extension `.csv`. Assurez-vous de remplacer `"chemin/vers/votre/fichier.csv"` par le chemin réel de votre fichier CSV.
+Récupération d'une Map à partir d'un fichier de configuration : Environ 1 jour
+Splitting de la Map et extraction de valeurs JSON : Environ 1 à 1.5 jours
+Affichage des attributs et de leurs valeurs dans les logs : Moins d'un jour
+Vérification des données sensibles : Environ 1 jour
+Intégration et tests finaux : Environ 1 jour
