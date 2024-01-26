1be4293d2bb6bc76169015094e8ce05b11796b88
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-01-26 15:58:00 +0100

    Update README.java

--- README.java
+++ README.java
@@ -15,14 +15,11 @@ attribut4="valeur4"
 # Le nombre de récurrences de attributA
 nombre_reccurences=$1
 
-# Générer la liste pour le cinquième attribut
-liste_attributA=()
-for ((i = 0; i < nombre_reccurences; i++)); do
-    liste_attributA+=("attributA")
-done
+# Créer le cinquième attribut comme une liste
+liste_attributA=$(jq -n "[{\"attributA\":\"valeurA\"} * $nombre_reccurences]")
 
 # Créer le fichier JSON avec les attributs
-echo "{\"attribut1\":\"$attribut1\", \"attribut2\":\"$attribut2\", \"attribut3\":\"$attribut3\", \"attribut4\":\"$attribut4\", \"liste_attributA\":$(jq -n --argjson vals "${liste_attributA[@]}" '$vals')}" > fichier.json
+echo "{\"attribut1\":\"$attribut1\", \"attribut2\":\"$attribut2\", \"attribut3\":\"$attribut3\", \"attribut4\":\"$attribut4\", \"liste_attributA\":$liste_attributA}" > fichier.json
 
 # Afficher le fichier JSON généré
 cat fichier.json
