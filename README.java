7d38aac23fb2e9d16864149ce223f055c271f846
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-02-08 15:05:24 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,45 @@
-#!/bin/bash
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.KeyManagerFactory;
+import java.security.KeyStore;
+import java.net.URL;
+
+public class HttpsClientExample {
+
+    public static void main(String[] args) {
+        try {
+            // Charger le keystore et le truststore
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(new FileInputStream("chemin/vers/keystore.jks"), "motDePasseKeystore".toCharArray());
+
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(new FileInputStream("chemin/vers/truststore.jks"), "motDePasseTruststore".toCharArray());
+
+            // Initialiser le KeyManagerFactory avec le keystore
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(keyStore, "motDePasseKeystore".toCharArray());
+
+            // Initialiser le TrustManagerFactory avec le truststore
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(trustStore);
+
+            // Initialiser le SSLContext avec les key managers et trust managers
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), new java.security.SecureRandom());
+
+            // Ouvrir la connexion
+            URL url = new URL("https://example.com");
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            conn.setSSLSocketFactory(sslContext.getSocketFactory());
+
+            // Effectuer la requête et lire la réponse
+            InputStream inputStream = conn.getInputStream();
+            // Lire inputStream pour obtenir la réponse...
+
+            System.out.println("Réponse obtenue.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
