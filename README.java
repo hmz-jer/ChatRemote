63ddb0dccbbb28c9d90f544d8a6c26d42510ccd6
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-02-09 09:30:27 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,35 +1,44 @@
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
 import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 
-public class DisableHostnameVerification {
+public static boolean sendJsonAndCheckFor413(String urlString, SSLContext sslContext, String jsonContent, boolean secureConnection) throws Exception {
+    URL url = new URL(urlString);
+    HttpURLConnection conn;
 
-    public static void main(String[] args) {
-        try {
-            // Créer un contexte SSL qui n'effectue pas de vérification du nom d'hôte
-            SSLContext sc = SSLContext.getInstance("TLS");
-            sc.init(null, null, new java.security.SecureRandom());
-
-            // Installer le gestionnaire d'hôtes qui ne vérifie pas les certificats
-            HostnameVerifier allHostsValid = new HostnameVerifier() {
-                public boolean verify(String hostname, SSLSession session) {
-                    return true; // Accepter tout nom d'hôte
-                }
-            };
-
-            // Appliquer les paramètres au niveau global (affecte toutes les connexions HttpsURLConnection)
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-
-            // À partir d'ici, toute connexion HttpsURLConnection ignorera la vérification du nom d'hôte
-            // Exemple d'utilisation :
-            HttpsURLConnection conn = (HttpsURLConnection) new URL("https://exemple.com").openConnection();
-            // Lire la réponse, etc.
-
-            System.out.println("Connexion réussie");
-        } catch (Exception e) {
-            e.printStackTrace();
+    if (secureConnection && url.getProtocol().equals("https")) {
+        conn = (HttpsURLConnection) url.openConnection();
+        
+        // Configurer le SSLContext, si fourni
+        if (sslContext != null) {
+            ((HttpsURLConnection)conn).setSSLSocketFactory(sslContext.getSocketFactory());
         }
+
+        // Désactiver la vérification du nom d'hôte
+        ((HttpsURLConnection)conn).setHostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true; // Accepter tout nom d'hôte
+            }
+        });
+
+    } else {
+        conn = (HttpURLConnection) url.openConnection();
     }
+
+    conn.setRequestMethod("POST");
+    conn.setRequestProperty("Content-Type", "application/json");
+    conn.setDoOutput(true);
+
+    try (DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
+        wr.writeBytes(jsonContent);
+        wr.flush();
+    }
+
+    int responseCode = conn.getResponseCode();
+    return responseCode == 413;
 }
