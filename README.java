8c77089df19b712f0c39231b503e4deb5f76f6fc
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-02-16 10:57:42 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,13 +1,23 @@
-Planification de mon congé parental pour naissance
+Voici un brouillon de votre e-mail basé sur les informations fournies :
 
-Bonjour Sorin,
+Objet : Résultats des Tests de Stress sur l'Environnement SI TOK
 
-J'espère que tu vas bien. Suite à notre discussion récente, je tenais à te fournir plus de détails concernant mon prochain congé parental. Comme discuté, ma femme est enceinte et nous attendons notre enfant pour la fin du mois de mars. La date théorique de l'accouchement est prévue entre le 20 et le 27 mars.
+Bonjour,
 
-Je prévois de prendre un congé parental de trois semaines à partir de la date de l'accouchement afin d'accueillir notre nouveau-né. 
+Nous vous écrivons pour partager les résultats des tests de stress que nous avons récemment effectués sur l'environnement SI TOK. L'objectif principal de ces tests était de déterminer la limite de la taille des messages que l'environnement peut traiter sans rencontrer l'erreur "Request Entity Too Large (413)".
 
-Étant donné la nature imprévisible de l'accouchement, je tenais à te prévenir que cela pourrait survenir avant la date prévue ,dans ce cas, je te contacterai dès que possible pour te tenir informé et ajuster mes dates de congé en conséquence.
+Pour ce faire, nous avons conçu et exécuté deux scénarios de test distincts, en veillant à ce que tous les messages envoyés soient sécurisés via un processus de signature et de chiffrement JWE/JWS.
+Scénario 1 : Nombre d'Opérations dans la Liste des Opérations
 
-Je te remercie par avance.
+Le premier scénario visait à déterminer le nombre maximal d'opérations que nous pouvions inclure dans la liste des opérations d'un message. Nous avons constaté que jusqu'à 67 opérations pouvaient être traitées avec succès lorsque la taille de l'attribut SData était fixée à 20 caractères alphanumériques.
+Scénario 2 : Taille de l'Attribut SData
 
-Bien cordialement.
+Dans le deuxième scénario, nous avons limité le nombre d'opérations à 50 et augmenté progressivement la taille de l'attribut SData. Il s'est avéré que la taille maximale de SData pouvait atteindre 63 caractères alphanumériques avant d'atteindre la limite qui déclenche l'erreur 413.
+
+Les deux scénarios ont été soigneusement documentés, et nous avons inclus des captures d'écran des résultats après chaque test pour illustrer nos conclusions.
+
+Ces tests révèlent des informations précieuses sur les capacités et les limites de l'environnement SI TOK, ce qui nous permettra d'optimiser nos intégrations futures et de prévoir les ajustements nécessaires pour éviter les dépassements de capacité.
+
+Nous restons à votre disposition pour toute question ou pour discuter plus en détail des résultats et de leurs implications.
+
+Cordialement,
