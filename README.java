3508879d28bad3c68d08776457c5408bc55a2f1d
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-02-23 11:50:06 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,49 +1,16 @@
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
-public class RecursiveFileParser {
-
-    public void parseFile(String filePath) throws IOException {
-        ObjectMapper objectMapper;
-        if (filePath.endsWith(".yaml") || filePath.endsWith(".yml")) {
-            objectMapper = new ObjectMapper(new YAMLFactory());
-        } else {
-            objectMapper = new ObjectMapper();
-        }
-
-        File file = new File(filePath);
-        Object content = objectMapper.readValue(file, Object.class);
-        processElement(content);
-    }
-
-    private void processElement(Object element) {
-        if (element instanceof Map) {
-            @SuppressWarnings("unchecked")
-            Map<String, Object> map = (Map<String, Object>) element;
-            map.forEach((key, value) -> {
-                System.out.println("Key: " + key);
-                processElement(value);
-            });
-        } else if (element instanceof List) {
-            @SuppressWarnings("unchecked")
-            List<Object> list = (List<Object>) element;
-            list.forEach(this::processElement);
-        } else {
-            System.out.println("Value: " + element);
-        }
-    }
-
-    public static void main(String[] args) {
-        RecursiveFileParser parser = new RecursiveFileParser();
-        try {
-            parser.parseFile("path/to/your/file.json"); // ou .yaml
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+Aprivate void processElement(Object element) {
+    if (element instanceof Map) {
+        Map<?, ?> originalMap = (Map<?, ?>) element;
+        // Convertit en ConcurrentHashMap si nécessaire pour le traitement spécifique
+        ConcurrentHashMap<String, Object> concurrentMap = new ConcurrentHashMap<>(originalMap);
+        concurrentMap.forEach((key, value) -> {
+            System.out.println("Key: " + key);
+            processElement(value);
+        });
+    } else if (element instanceof List) {
+        List<?> list = (List<?>) element;
+        list.forEach(this::processElement);
+    } else {
+        System.out.println("Value: " + element);
     }
 }
