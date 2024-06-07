3101d5646429ade32aac6370d96134daa0963f55
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-06-07 14:59:24 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,28 @@
-qsd
+
+@Override
+public void run() {
+    try {
+        status = AvailableStatus.STARTED;
+        RsLogEnum.KAFKA_CONSUMER_STARTED.generateLog();
+
+        synchronized (this) {
+            while (!stopped) {
+                while (paused) {
+                    wait(); // Attendre que paused soit mis à false
+                }
+                processTopic();
+            }
+        }
+    } catch (InterruptedException e) {
+        LOGGER.error("Consumer thread interrupted", e);
+        RsLogEnum.KAFKA_POLLING_THREAD_ERROR.generateLog(e.getMessage());
+        Thread.currentThread().interrupt();
+    } catch (Exception e) {
+        LOGGER.error("Global consuming error", e);
+        RsLogEnum.KAFKA_POLLING_THREAD_ERROR.generateLog(e.getMessage());
+    } finally {
+        status = AvailableStatus.STOPPED;
+        RsLogEnum.KAFKA_CONSUMER_STOPPED.generateLog();
+        consumer.close();
+    }
+}
