b892c66eb51777a49e0850494bc8397540327fd6
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-06-13 14:52:16 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,60 @@
-opm
+ apply plugin: 'java'
+apply plugin: 'application'
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation 'org.bouncycastle:bcprov-jdk15on:1.68'
+    testImplementation 'junit:junit:4.12'
+}
+
+sourceSets {
+    server {
+        java {
+            srcDirs = ['src/main/java/server']
+        }
+        resources {
+            srcDirs = ['resources/server']
+        }
+    }
+    client {
+        java {
+            srcDirs = ['src/main/java/client']
+        }
+        resources {
+            srcDirs = ['resources/client']
+        }
+    }
+}
+
+task createServerJar(type: Jar) {
+    archiveBaseName.set('server-socket')
+    manifest {
+        attributes(
+            'Main-Class': 'server.ServerSocket'
+        )
+    }
+    from {
+        configurations.runtimeClasspath.collect { it.isDirectory() ? it : zipTree(it) }
+    }
+    from sourceSets.server.output
+    from sourceSets.server.resources
+}
+
+task createClientJar(type: Jar) {
+    archiveBaseName.set('client')
+    manifest {
+        attributes(
+            'Main-Class': 'client.Client'
+        )
+    }
+    from {
+        configurations.runtimeClasspath.collect { it.isDirectory() ? it : zipTree(it) }
+    }
+    from sourceSets.client.output
+    from sourceSets.client.resources
+}
+
+tasks.build.dependsOn(createServerJar, createClientJar)
