8a2b3a330e5a9ea3ab05c177dd74d42496678177
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-07-05 17:13:06 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,21 +1,11 @@
-
-Mise en Place de l'API Gateway
-
-        Mise en place de l'API Gateway avec le même code que le projet digital
-        Configuration des endpoints et des routes nécessaires pour communiquer avec le backend
-
-Intégration de l'API Gateway avec le Reverse Proxy
-
-   
-        Configuration du reverse proxy pour diriger les requêtes vers l'API Gateway
-        Test de l'intégration pour assurer la bonne communication
-		
-Développement du Module RSLOG
-
-        Intégration du module avec les différents composants de l'application pour enregistrer les logs
-		
-Mise à jour de WS-simulateur
-
-
-        Mise à jour de l'outil WS-simulateur pour permettre les tests des requêtes et des réponses.
-        Ajout des fonctionnalités nécessaires pour simuler les différents scénarios de communication avec l'API Gateway et Kafka.
+   Map<MetricName, ? extends Metric> metrics = consumer.metrics();
+            for (MetricName metricName : metrics.keySet()) {
+                String name = metricName.name();
+                if (name.equals("connection-count") || name.equals("incoming-byte-rate") || name.equals("outgoing-byte-rate") || name.equals("request-rate") || name.equals("response-rate")) {
+                    Metric metric = metrics.get(metricName);
+                    double value = (Double) metric.metricValue();
+                    if (value > 0) {
+                        return true; // Kafka est accessible
+                    }
+                }
+            }
