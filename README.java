fc3f57f540071013463ec69f67b6eff081606052
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-07-08 09:45:30 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,11 +1,11 @@
-   Map<MetricName, ? extends Metric> metrics = consumer.metrics();
-            for (MetricName metricName : metrics.keySet()) {
-                String name = metricName.name();
-                if (name.equals("connection-count") || name.equals("incoming-byte-rate") || name.equals("outgoing-byte-rate") || name.equals("request-rate") || name.equals("response-rate")) {
-                    Metric metric = metrics.get(metricName);
-                    double value = (Double) metric.metricValue();
-                    if (value > 0) {
-                        return true; // Kafka est accessible
-                    }
-                }
-            }
+Bonjour à tous,
+
+Je tiens à vous informer que le problème de lenteur rencontré au niveau de la commande manage.sh status a été résolu.
+
+Nous avons remplacé l'utilisation de Kafka Admin par Kafka Metrics. Cette modification nous permet d'obtenir une réponse plus rapide et nous évite de créer un Kafka Admin à chaque exécution de la commande.
+
+Nous allons continuer à tester cette solution sur l'environnement de développement (DEV) afin de nous assurer de sa stabilité et de son efficacité avant de la livrer en production.
+
+Merci de votre patience et de votre compréhension.
+
+Cordialement,
