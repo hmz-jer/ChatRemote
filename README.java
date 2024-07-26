549a0c97f10418861a1f94369b7962c46408ec90
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-07-26 10:48:36 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,13 +1,1 @@
-Bonjour,
 
-Je vous écris pour vous faire part des points suivants suite à la migration IAG :
-
-    Initialement, la migration était prévue pour jeudi. Cependant, mes identifiants de Pulse et ceux d’Outlook ont été changés dès mercredi après-midi, ce qui a bloqué mon accès à ces services pour le reste de la journée de mercredi.
-    J'ai rencontré des problèmes sur Teams après la migration. Heureusement, ce problème a été résolu rapidement.
-    Chaque matin, il faut plusieurs tentatives pour réussir à se connecter à Pulse, ce qui entraîne une perte de temps et d’efficacité.
-
-Je sollicite votre aide pour résoudre le dernier point mentionné afin de pouvoir reprendre mes activités normalement.
-
-Merci de votre compréhension et de votre assistance.
-
-Cordialement,
