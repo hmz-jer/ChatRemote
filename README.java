209476186a6f6571be1d6dde56510b07dda4f427
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-07-26 10:10:18 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,13 @@
-  Questions Supplémentaires (suite)Utilisation de Docker : Est-ce que nous devrions envisager d'utiliser Docker pour containeriser notre application et faciliter le déploiement ?Dossier d'Intégration : Est-ce que nous devons également ajouter un dossier integration contenant un script manage.sh, comme c'est le cas dans nos autres applications, pour gérer les opérations d'intégration ?Utilisation de rsyslog : Est-ce que nous prévoyons d'utiliser rsyslog pour la gestion et la centralisation des logs de notre application ?
+Bonjour,
+
+Je vous écris pour vous faire part des points suivants suite à la migration IAG :
+
+    Initialement, la migration était prévue pour jeudi. Cependant, mes identifiants de Pulse et ceux d’Outlook ont été changés dès mercredi après-midi, ce qui a bloqué mon accès à ces services pour le reste de la journée de mercredi.
+    J'ai rencontré des problèmes sur Teams après la migration. Heureusement, ce problème a été résolu rapidement.
+    Chaque matin, il faut plusieurs tentatives pour réussir à se connecter à Pulse, ce qui entraîne une perte de temps et d’efficacité.
+
+Je sollicite votre aide pour résoudre le dernier point mentionné afin de pouvoir reprendre mes activités normalement.
+
+Merci de votre compréhension et de votre assistance.
+
+Cordialement,
