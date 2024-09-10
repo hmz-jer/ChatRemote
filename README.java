99c2d167230d386eefd63384a042b88ce3c83b65
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-09-10 10:49:52 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,16 +1,9 @@
 
 # Configuration LDAP
 spring.ldap.urls=ldap://localhost:389
-spring.ldap.base=dc=interne,dc=carte,dc=com
-spring.ldap.username=cn=admin,dc=interne,dc=carte,dc=com
+spring.ldap.base=dc=interne,dc=cartes,dc=com
+spring.ldap.username=cn=admin,dc=interne,dc=cartes,dc=com
 spring.ldap.password=admin_password
-
-# Configuration de l'authentification LDAP
-spring.security.ldap.authentication-strategy=simple
-spring.security.ldap.user-search-base=ou=users
-spring.security.ldap.user-search-filter=(uid={0})
-spring.security.ldap.group-search-base=ou=groups
-spring.security.ldap.group-search-filter=(member={0})
 
 # Activer la sécurité de Spring
 spring.security.enabled=true
