796421e341e5960bdeb62d988067d1c175494597
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-09-13 16:19:25 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,2 +1,15 @@
-#!/bin/bash
-ldapmodify -Y EXTERNAL -H ldapi:/// -f /container/service/slapd/assets/config/bootstrap/ldif/custom/custom.ldif
+    volumes:
+      - ./ldap_data:/var/lib/ldap
+      - ./ldap_config:/etc/ldap/slapd.d
+      - ./ldap/init.ldif:/container/service/slapd/assets/config/bootstrap/ldif/50-bootstrap.ldif
+      - ./bootstrap:/container/service/slapd/assets/config/bootstrap/ldif/custom
+    command: 
+      - --copy-service
+      - --loglevel debug
+    entrypoint: 
+      - /bin/sh
+      - -c
+      - |
+        /container/tool/run --copy-service --loglevel debug &
+        sleep 10
+        /container/service/slapd/assets/config/bootstrap/ldif/custom/setup.sh
