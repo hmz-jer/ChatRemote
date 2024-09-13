d0f441c5cd10121d158815135db17ebc3663728f
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-09-13 16:55:30 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,6 +1,6 @@
-dn: olcDatabase={1}mdb,cn=config
+dn: olcDatabase={2}mdb,cn=config
 changetype: modify
-add: olcAccess
-olcAccess: to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * break
-olcAccess: to attrs=userPassword,shadowLastChange by self write by anonymous auth by dn="cn=admin,dc=interne,dc=cartes,dc=com" write by * none
-olcAccess: to * by self read by dn="cn=admin,dc=interne,dc=cartes,dc=com" write by anonymous read by * none
+replace: olcAccess
+olcAccess: {0}to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * break
+olcAccess: {1}to attrs=userPassword,shadowLastChange by self write by anonymous auth by * none
+olcAccess: {2}to * by self read by anonymous read by * none
