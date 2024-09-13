794101b3eb334836ee61f4ed1d9523ccb0f44568
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-09-13 16:15:38 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,6 +1,2 @@
-dn: olcDatabase={1}mdb,cn=config
-changetype: modify
-add: olcAccess
-olcAccess: to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * break
-olcAccess: to attrs=userPassword,shadowLastChange by self write by anonymous auth by dn="cn=admin,dc=interne,dc=cartes,dc=com" write by * none
-olcAccess: to * by self read by dn="cn=admin,dc=interne,dc=cartes,dc=com" write by anonymous read by * none
+#!/bin/bash
+ldapmodify -Y EXTERNAL -H ldapi:/// -f /container/service/slapd/assets/config/bootstrap/ldif/custom/custom.ldif
