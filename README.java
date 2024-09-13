16e1fe5db911e5c4b9ec96f84166d9e2f1cce835
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-09-13 11:50:19 +0200

    Update README.java

--- README.java
+++ README.java
@@ -2,13 +2,7 @@ ldapmodify -Y EXTERNAL -H ldapi:/// <<EOF
 dn: olcDatabase={1}mdb,cn=config
 changetype: modify
 replace: olcAccess
-olcAccess: to attrs=userPassword
-  by anonymous auth
-  by self write
-  by * none
+olcAccess: to attrs=userPassword by anonymous read by self write by * none
 olcAccess: to dn.base="" by * read
-olcAccess: to *
-  by self write
-  by anonymous read
-  by * read
+olcAccess: to * by self write by anonymous read by * read
 EOF
