ceb90aa8f55547160e5cda2ae253061dc3655cc2
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-09-16 11:19:01 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,6 +1,8 @@
-dn: olcDatabase={2}mdb,cn=config
+ldapmodify -Y EXTERNAL -H ldapi:/// << EOF
+dn: olcDatabase={1}mdb,cn=config
 changetype: modify
 replace: olcAccess
 olcAccess: {0}to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * break
-olcAccess: {1}to attrs=userPassword,shadowLastChange by self write by anonymous auth by * none
+olcAccess: {1}to attrs=userPassword by self write by anonymous auth by * none
 olcAccess: {2}to * by self read by anonymous read by * none
+EOF
