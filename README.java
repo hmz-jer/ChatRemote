98b9c0a0d7284b9ff343f9a685d2f717eb867c2f
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-09-16 11:29:53 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,7 +1,7 @@
-ldapmodify -Y EXTERNAL -H ldapi:/// << EOF
+ldapmodify -x -D "cn=admin,dc=interne,dc=cartes,dc=com" -W -H ldap:/// << EOF
 dn: olcDatabase={1}mdb,cn=config
 changetype: modify
 replace: olcAccess
 olcAccess: {0}to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * break
-olcAccess: {1}to * by self write by anonymous auth by * none
+olcAccess: {1}to * by self write by anonymous auth by * read
 EOF
