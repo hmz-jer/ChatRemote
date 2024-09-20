970276dec4c663d0dbe1f437fc6a11b39592f9c6
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-09-20 14:54:41 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,7 +1,54 @@
-ldapmodify -Y EXTERNAL -H ldapi:/// << EOF
-dn: olcDatabase={1}mdb,cn=config
-changetype: modify
-replace: olcAccess
-olcAccess: {0}to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * break
-olcAccess: {1}to * by self write by anonymous auth by * read
-EOF
+ # [Nom du Projet]
+
+## Table des matières
+1. [Prérequis](#prérequis)
+2. [Structure du projet](#structure-du-projet)
+3. [Lancement de l'application](#lancement-de-lapplication)
+4. [Exécution des tests](#exécution-des-tests)
+5. [Fonctionnalités réutilisables du socle Oxc](#fonctionnalités-réutilisables-du-socle-oxc)
+
+## Prérequis
+[Gardez cette section telle quelle, en listant tous les prérequis nécessaires pour exécuter l'application]
+
+## Structure du projet
+[Conservez cette section telle quelle, en décrivant la structure actuelle du projet]
+
+## Lancement de l'application
+[Gardez les instructions existantes pour lancer l'application Spring Boot]
+
+## Exécution des tests
+[Conservez les instructions existantes pour exécuter les tests]
+
+## Fonctionnalités réutilisables du socle Oxc
+
+Ce projet fait partie du socle Oxc et implémente plusieurs fonctionnalités réutilisables qui peuvent être utilisées dans d'autres projets du socle. Voici un aperçu de ces fonctionnalités :
+
+### 1. Structure de projet Spring Boot Angular
+- Description : Template de base pour les projets combinant Spring Boot et Angular.
+- Utilisation : Peut être utilisé comme point de départ pour de nouveaux projets.
+
+### 2. Sécurité JWT
+- Description : Implémentation de l'authentification et de l'autorisation basées sur JWT.
+- Utilisation : Peut être intégrée dans d'autres projets nécessitant une sécurité robuste.
+
+### 3. Configuration Open API
+- Description : Mise en place de la documentation API automatique avec Open API.
+- Utilisation : Peut être adoptée par d'autres projets pour standardiser la documentation API.
+
+### 4. Configuration LDAP et PostgreSQL
+- Description : Configurations pour l'intégration de LDAP et PostgreSQL.
+- Utilisation : Peut être réutilisée dans les projets nécessitant ces technologies.
+
+### 5. Scripts Docker
+- Description : Configurations Docker pour PostgreSQL et OpenLDAP.
+- Utilisation : Peut être utilisée comme base pour d'autres projets nécessitant ces services.
+
+### 6. Spring Actuator
+- Description : Configuration des endpoints de monitoring et de gestion.
+- Utilisation : Peut être appliquée à d'autres projets Spring Boot pour standardiser le monitoring.
+
+### 7. Script manage.sh
+- Description : Script pour gérer le démarrage, l'arrêt et le statut de l'application.
+- Utilisation : Peut être adapté pour d'autres projets Java du socle.
+
+Pour plus de détails sur l'utilisation de ces fonctionnalités dans d'autres projets, veuillez consulter la documentation du socle Oxc ou contacter l'équipe de développement.
