6a6a2b26eeebcba7cfa4e841a3d1dcbbc850e951
hmz <5492689+hmz-jer@users.noreply.github.com>
2024-12-13 17:29:31 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,91 +1,56 @@
  import org.apache.commons.text.StringEscapeUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class LoggingUtils {
-    private static boolean printOutEvents = false;
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingUtils.class);
-    
-    public static void setPrintEvents(boolean value) {
-        printOutEvents = value;
-    }
-    
-    public static void loggingEvent(RSLogger logger, LogEvent event, String... args) {
-        if (logger != null) {
-            // Sanitize tous les arguments
-            String[] sanitizedArgs = sanitizeArgs(args);
-            
-            // Log avec le logger spécifique
-            logger.log(event, sanitizedArgs);
-            
-            // Log additionnel si printOutEvents est activé
-            if (printOutEvents && event != null) {
-                String sanitizedEventLabel = sanitizeString(event.getLabel());
-                String sanitizedArgsStr = formatArgs(sanitizedArgs);
-                LOGGER.info("{}: {}", sanitizedEventLabel, sanitizedArgsStr);
-            }
-        }
-    }
-    
-    public static void loggingEvent(RSLogger logger, LogEvent event, 
-            String args1, String args2) {
-        if (logger != null) {
-            // Sanitize les arguments individuels
-            String sanitizedArgs1 = sanitizeString(args1);
-            String sanitizedArgs2 = sanitizeString(args2);
-            
-            // Log avec le logger spécifique
-            logger.log(event, sanitizedArgs1, sanitizedArgs2);
-            
-            // Log additionnel si printOutEvents est activé
-            if (printOutEvents && event != null) {
-                String sanitizedEventLabel = sanitizeString(event.getLabel());
-                LOGGER.info("{}: {} {}", 
-                    sanitizedEventLabel, 
-                    sanitizedArgs1,
-                    sanitizedArgs2);
-            }
-        }
-    }
-    
-    private static String[] sanitizeArgs(String... args) {
-        if (args == null) {
-            return new String[0];
-        }
-        
-        return java.util.Arrays.stream(args)
-            .map(LoggingUtils::sanitizeString)
-            .toArray(String[]::new);
-    }
-    
-    private static String sanitizeString(String input) {
+public class StringSanitizer {
+    private static final int MAX_LENGTH = 1000;
+
+    /**
+     * Sanitize une chaîne de caractères pour le logging
+     */
+    public static String sanitize(String input) {
         if (input == null) {
             return "null";
         }
-        
-        // Supprimer les caractères de contrôle
+
+        // Suppression des caractères de contrôle
         String sanitized = input.replaceAll("[\\r\\n\\t]", " ");
         
-        // Échapper les caractères spéciaux
+        // Échappement des caractères spéciaux
         sanitized = StringEscapeUtils.escapeJava(sanitized);
         
-        // Limiter la longueur
-        return truncateIfNeeded(sanitized);
+        // Troncature si nécessaire
+        return truncate(sanitized);
     }
-    
-    private static String formatArgs(String[] args) {
-        if (args == null || args.length == 0) {
-            return "[]";
+
+    /**
+     * Sanitize un tableau de chaînes
+     */
+    public static String[] sanitizeArray(String... inputs) {
+        if (inputs == null) {
+            return new String[0];
         }
-        
-        return "[" + String.join(", ", args) + "]";
+
+        return java.util.Arrays.stream(inputs)
+            .map(StringSanitizer::sanitize)
+            .toArray(String[]::new);
     }
-    
-    private static String truncateIfNeeded(String input) {
-        final int MAX_LENGTH = 1000;
+
+    /**
+     * Tronque une chaîne si elle dépasse la longueur maximale
+     */
+    public static String truncate(String input) {
         if (input != null && input.length() > MAX_LENGTH) {
             return input.substring(0, MAX_LENGTH) + "...";
         }
         return input;
+    }
+
+    /**
+     * Formate un tableau de chaînes en une représentation lisible
+     */
+    public static String formatArray(String[] array) {
+        if (array == null || array.length == 0) {
+            return "[]";
+        }
+        return "[" + String.join(", ", array) + "]";
     }
 }
