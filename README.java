a7a4c5d8744d0883c7a57c597759d7271781a457
hmz <5492689+hmz-jer@users.noreply.github.com>
2025-01-15 15:36:21 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,13 @@
  
+/ Configuration OpenAPI
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Kafka Request-Reply")
+                        .version("1.0.0")
+                        .description("API de traitement asynchrone des requêtes via Kafka")
+                        .contact(new Contact()
+                                .name("Votre Nom")
+                                .email("contact@example.com")));
+    }
