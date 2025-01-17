275705fc3b3f235580bd0e2144990685646a1660
hmz <5492689+hmz-jer@users.noreply.github.com>
2025-01-17 16:33:38 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,61 +1,8 @@
-# 1. Créer un répertoire pour les certificats
-mkdir -p certs
-cd certs
-
-# 2. Créer le keystore client
-keytool -keystore kafka.client.keystore.jks \
-    -alias client \
-    -validity 365 \
-    -genkey \
-    -keyalg RSA \
-    -storepass azerty \
-    -keypass azerty \
-    -dname "CN=client-test-local" \
-    -storetype PKCS12
-
-# 3. Créer la demande de signature de certificat (CSR)
-keytool -keystore kafka.client.keystore.jks \
-    -alias client \
-    -certreq \
-    -file client.csr \
-    -storepass azerty \
-    -keypass azerty \
-    -storetype PKCS12
-
-# 4. Signer le certificat client avec le CA
-# (assurez-vous que ca.crt et ca.key sont présents)
-openssl x509 -req \
-    -CA ca.crt \
-    -CAkey ca.key \
-    -in client.csr \
-    -out client.crt \
-    -days 365 \
-    -CAcreateserial \
-    -passin pass:azerty
-
-# 5. Importer le certificat CA dans le keystore client
-keytool -keystore kafka.client.keystore.jks \
-    -alias CARoot \
-    -import \
-    -file ca.crt \
-    -storepass azerty \
-    -noprompt \
-    -storetype PKCS12
-
-# 6. Importer le certificat client signé dans le keystore
-keytool -keystore kafka.client.keystore.jks \
-    -alias client \
-    -import \
-    -file client.crt \
-    -storepass azerty \
-    -noprompt \
-    -storetype PKCS12
-
-# 7. Créer le truststore client
-keytool -keystore kafka.client.truststore.jks \
-    -alias CARoot \
-    -import \
-    -file ca.crt \
-    -storepass azerty \
-    -noprompt \
-    -storetype PKCS12
+spring.kafka.bootstrap-servers=IP_SERVEUR_KAFKA:9093
+spring.kafka.properties.security.protocol=SSL
+spring.kafka.properties.ssl.truststore.location=./certs/kafka.client.truststore.jks
+spring.kafka.properties.ssl.truststore.password=azerty
+spring.kafka.properties.ssl.keystore.location=./certs/kafka.client.keystore.jks
+spring.kafka.properties.ssl.keystore.password=azerty
+spring.kafka.properties.ssl.key.password=azerty
+spring.kafka.properties.ssl.endpoint.identification.algorithm=
