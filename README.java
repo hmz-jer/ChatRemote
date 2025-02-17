dbf0af48001851ebd9def42157815ea78d7ad72b
hmz <5492689+hmz-jer@users.noreply.github.com>
2025-02-17 14:22:09 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,10 @@
 
+simulation:
+  log_level: "DEBUG"
+
+scenarios:
+  K1:
+    request_file: "requests/K1_request.json"
+    request_endpoint: "http://localhost:8081/src/K9"
+    expected_response: "responses/K1_response.json"
+    
