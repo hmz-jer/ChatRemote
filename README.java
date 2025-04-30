51bd0a0b69c24f40fcd1523b223df6c0d0736c4a
hmz <5492689+hmz-jer@users.noreply.github.com>
2025-04-30 10:54:00 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,10 +1,13 @@
- 
-- La génération de certificat **n'est pas un point bloquant** pour Axway
-  - Ils peuvent avancer avec leurs propres certificats générés
-  - Nous avançons également avec nos certificats de notre côté
+ Bonjour,
 
-- Le développement du **use case outbound** progresse normalement
-  - Les spécifications sont claires
-  - Aucun point de blocage identifié
+Point d'avancement :
+- Poursuite des travaux sur le proxy Java
+- Axway travaille sur le flux outbound et organise un atelier de restitution cet après-midi
+- L'équipe build doit commencer à planifier l'importation des certificats Vops dans Cassandra via un job Jenkins
 
-- En parallèle, collaboration active avec l'**équipe build** pour la partie CI/CD du proxy
+Autre sujet :
+- RDV prévu avec Hafid la semaine prochaine
+- Objectif : expliquer le fonctionnement de ws-simulateur et répondre à ses questions
+
+Cordialement,
+[Votre nom]
