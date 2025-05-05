324e0876b24e2b6241771154f6d02cd3816917fc
hmz <5492689+hmz-jer@users.noreply.github.com>
2025-05-05 17:31:37 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,50 +1,19 @@
- // build.gradle
-plugins {
-    id 'org.springframework.boot' version '3.1.5'
-    id 'io.spring.dependency-management' version '1.1.3'
-    id 'java'
-}
+ package com.example.kafkamock;
 
-group = 'com.example'
-version = '0.0.1-SNAPSHOT'
-sourceCompatibility = '17'
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-configurations {
-    compileOnly {
-        extendsFrom annotationProcessor
+/**
+ * Classe principale du mock Kafka pour tester le proxy Java
+ * Cette application permet d'envoyer des messages sur un topic Kafka et de recevoir les réponses
+ * sur un autre topic pour valider le fonctionnement du proxy Java.
+ */
+@SpringBootApplication
+@EnableScheduling
+public class KafkaMockApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaMockApplication.class, args);
     }
 }
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation 'org.springframework.boot:spring-boot-starter'
-    implementation 'org.springframework.boot:spring-boot-starter-web'
-    implementation 'org.springframework.boot:spring-boot-starter-actuator'
-    implementation 'org.springframework.kafka:spring-kafka'
-    implementation 'com.fasterxml.jackson.core:jackson-databind'
-    implementation 'com.fasterxml.jackson.datatype:jackson-datatype-jsr310'
-    
-    compileOnly 'org.projectlombok:lombok'
-    annotationProcessor 'org.projectlombok:lombok'
-    
-    testImplementation 'org.springframework.boot:spring-boot-starter-test'
-    testImplementation 'org.springframework.kafka:spring-kafka-test'
-}
-
-test {
-    useJUnitPlatform()
-}
-
-bootJar {
-    archiveFileName = "kafka-mock-${version}.jar"
-}
-
-// settings.gradle
-rootProject.name = 'kafka-mock'
-
-// gradle.properties
-org.gradle.jvmargs=-Xmx2048m
-org.gradle.parallel=true
