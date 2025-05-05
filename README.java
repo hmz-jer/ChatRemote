762ea13c261bdc3a144dffdd5f7217de1b5291a7
hmz <5492689+hmz-jer@users.noreply.github.com>
2025-05-05 17:20:06 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,13 +1,50 @@
- Bonjour,
+ // build.gradle
+plugins {
+    id 'org.springframework.boot' version '3.1.5'
+    id 'io.spring.dependency-management' version '1.1.3'
+    id 'java'
+}
 
-Point d'avancement :
-- Poursuite des travaux sur le proxy Java
-- Axway travaille sur le flux outbound et organise un atelier de restitution cet après-midi
-- L'équipe build doit commencer à planifier l'importation des certificats Vops dans Cassandra via un job Jenkins
+group = 'com.example'
+version = '0.0.1-SNAPSHOT'
+sourceCompatibility = '17'
 
-Autre sujet :
-- RDV prévu avec Hafid la semaine prochaine
-- Objectif : expliquer le fonctionnement de ws-simulateur et répondre à ses questions
+configurations {
+    compileOnly {
+        extendsFrom annotationProcessor
+    }
+}
 
-Cordialement,
-[Votre nom]
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation 'org.springframework.boot:spring-boot-starter'
+    implementation 'org.springframework.boot:spring-boot-starter-web'
+    implementation 'org.springframework.boot:spring-boot-starter-actuator'
+    implementation 'org.springframework.kafka:spring-kafka'
+    implementation 'com.fasterxml.jackson.core:jackson-databind'
+    implementation 'com.fasterxml.jackson.datatype:jackson-datatype-jsr310'
+    
+    compileOnly 'org.projectlombok:lombok'
+    annotationProcessor 'org.projectlombok:lombok'
+    
+    testImplementation 'org.springframework.boot:spring-boot-starter-test'
+    testImplementation 'org.springframework.kafka:spring-kafka-test'
+}
+
+test {
+    useJUnitPlatform()
+}
+
+bootJar {
+    archiveFileName = "kafka-mock-${version}.jar"
+}
+
+// settings.gradle
+rootProject.name = 'kafka-mock'
+
+// gradle.properties
+org.gradle.jvmargs=-Xmx2048m
+org.gradle.parallel=true
