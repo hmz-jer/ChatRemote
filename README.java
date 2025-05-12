31acd9057b913f322241629377a62ba1021e2775
hmz <5492689+hmz-jer@users.noreply.github.com>
2025-05-12 09:54:12 +0100

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,14 @@
- Le monsieur est repassé tout à l’heure et m’a échangé le badge contre un badge anonyme (sans nom ni prénom), le temps qu’il corrige correctement mon nom.
+Objet : Demande de création d'un nouveau dépôt GitLab - mock-vop-client
+
+Bonjour,
+
+Je souhaite faire une demande de création d'un nouveau dépôt GitLab avec les caractéristiques suivantes :
+
+Nom du dépôt : mock-vop-client
+Description : Simulateur de banques pour la validation des certificats QWAC dans le cadre du projet iban check
+Technologie : Java/Gradle
+Projet parent : iban check
+
+Ce composant sera utilisé pour mocker les interactions avec les banques afin de nous permettre de valider les certificats QWAC dans un environnement contrôlé.
+
+Merci de bien vouloir procéder à la création de ce dépôt.
