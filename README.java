da0d2dd2475d74d796c122c231eab106bbdae93e
hmz <5492689+hmz-jer@users.noreply.github.com>
2025-06-27 16:29:15 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,19 +1,11 @@
-version: '3.8'
-services:
-  kafka-ui:
-    container_name: kafka-ui
-    image: provectuslabs/kafka-ui:latest
-    ports:
-      - "8080:8080"
-    environment:
-      KAFKA_CLUSTERS_0_NAME: mon-cluster-ssl
-      KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS: kafka-broker:9093
-      KAFKA_CLUSTERS_0_PROPERTIES_SECURITY_PROTOCOL: SSL
-      KAFKA_CLUSTERS_0_PROPERTIES_SSL_TRUSTSTORE_LOCATION: /etc/kafka/secrets/kafka.client.truststore.jks
-      KAFKA_CLUSTERS_0_PROPERTIES_SSL_TRUSTSTORE_PASSWORD: changeit
-      # Pour SSL mutuel
-      KAFKA_CLUSTERS_0_PROPERTIES_SSL_KEYSTORE_LOCATION: /etc/kafka/secrets/kafka.client.keystore.jks
-      KAFKA_CLUSTERS_0_PROPERTIES_SSL_KEYSTORE_PASSWORD: changeit
-      KAFKA_CLUSTERS_0_PROPERTIES_SSL_KEY_PASSWORD: changeit
-    volumes:
-      - ./secrets:/etc/kafka/secrets:ro
+ Oui, excellente idée ! Tester avec curl va vous aider à identifier le problème. Voici comment procéder selon le format de votre truststore :Avec un truststore JKS1. Convertir le JKS en PEM d'abord :keytool -exportcert -keystore truststore.jks -alias votre_alias -file cert.pem -rfc
+# Ou pour exporter tous les certificats
+keytool -list -keystore truststore.jks -v2. Test curl avec le certificat PEM :curl -v --cacert cert.pem https://votreserveur.com/votre-endpointAvec un truststore PEM directementcurl -v --cacert truststore.pem https://votreserveur.com/votre-endpointAvec un certificat p12/pfx# Convertir d'abord en PEM
+openssl pkcs12 -in truststore.p12 -out truststore.pem -nodes
+
+# Puis tester
+curl -v --cacert truststore.pem https://votreserveur.com/votre-endpointTests de diagnostic1. Test sans validation SSL (pour voir si c'est bien un problème de certificat) :curl -k -v https://votreserveur.com/votre-endpoint2. Test avec informations détaillées :curl -v --cacert cert.pem https://votreserveur.com/votre-endpoint 2>&1 | grep -E "(SSL|TLS|certificate|subject|issuer)"3. Voir les certificats du serveur :openssl s_client -connect votreserveur.com:443 -showcertsExemple de test POSTcurl -v --cacert cert.pem \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"test": "data"}' \
+  https://votreserveur.com/votre-endpointQuel format a votre truststore ? Et pouvez-vous partager l'URL que vous testez (sans données sensibles) ?
