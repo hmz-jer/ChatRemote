56cd39fbd036cdd704c86e47561142f11242859b
hmz <5492689+hmz-jer@users.noreply.github.com>
2025-07-16 11:45:50 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,25 +1,3 @@
- Bonjour,
+  Je souhaite vous demander une autorisation d’absence de 2 heures le [date], de 10h à 12h, afin de récupérer mon titre de séjour à la préfecture.
 
-Axway nous a fourni l’archive .7z en attendant la publication officielle de la version 1.14.12.
-À ce jour, la dernière version disponible sur leur repository officiel reste la 1.14.11 :
-👉 https://github.com/Axway/apimanager-cookbook/releases
-
-La version 1.14.12 mentionnée dans le script d'onboarding n’est donc pas encore accessible autrement.
-Nous utilisons temporairement l’archive transmise, en veillant à nettoyer le répertoire DEV dès que la version officielle sera disponible.
-
-Cordialement,
-
-#####@###
-
-     Bonjour,
-
-Pour information, Axway nous a transmis l’archive .7z de la version 1.14.12 en attendant sa publication officielle.
-À ce jour, la dernière version disponible sur leur repository GitHub est toujours la 1.14.11 :
-👉 https://github.com/Axway/apimanager-cookbook/releases
-
-La version utilisée dans le script d’onboarding (1.14.12) n’est donc pas encore récupérable autrement.
-Nous restons bien entendu vigilants sur le nettoyage du répertoire DEV une fois la version officielle en ligne.
-
-Si vous avez d’autres propositions ou une meilleure approche, on est preneurs !
-
-Bien cordialement, 
+Je prévois de commencer plus tôt ce jour-là pour compenser cette absence et m’assurer que cela n’impacte pas mon travail.
