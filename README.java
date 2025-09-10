d480ba88d011ea7efa730be17a2f6d2817c267e7
hmz <5492689+hmz-jer@users.noreply.github.com>
2025-09-10 10:22:18 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,2 +1,33 @@
-La version actuelle pgAdmin 8.5 n’est pas compatible avec PostgreSQL et pose des problèmes.
-Merci d’installer la version 8.13 sur ma machine (IP : <ton IP>).
+ Remarques / Points à corriger dans le document
+
+1. Versions à préciser
+
+Indiquer la version de pgAdmin et Postgres à installer.
+
+Créer un ticket SID pour cette demande.
+
+
+
+2. Environnement de développement
+
+Préciser que l’on doit utiliser la version Ultimate de IntelliJ pour configurer Tomcat.
+
+
+
+3. Version Java
+
+Indiquer la version Java utilisée dans ce projet.
+
+
+
+4. Configuration application
+
+Vérifier l’URL de l’application et le port : les valeurs actuelles ne sont pas correctes dans le document.
+
+
+
+5. Scripts SQL
+
+Expliquer l’utilisation du script update_version.sql (il doit être mis à jour à chaque modification SQL).
+
+
